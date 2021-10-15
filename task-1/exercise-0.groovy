6767e7869681e5d677774c9be000e11ebc697f96
Davvidos <dawidjurczynski@gmail.com>
2021-10-15 22:22:16 +0200

    task-1

---
+++ task-1/exercise-0.groovy
@@ -0,0 +1,7 @@
+// Napisać w wybranym edytorze tekstowym program typu "hello world" w Groovy. Uruchomić go z konsoli.
+
+class task_1_exercise_0 {
+    static void main(String[] args) {
+        println("Hello world!")
+    }
+}
