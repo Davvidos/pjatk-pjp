654e43819083749fe69a5dd45ae6b829cae99bb5
Davvidos <dawidjurczynski@gmail.com>
2021-10-29 16:10:13 +0200

    Task 2

---
+++ task-2/exercise-5.groovy
@@ -0,0 +1,27 @@
+// Zapewnić iterowanie na Iterable za pomocą metody eachTuple, umożliwiającej działanie podanego domknięcia na kolejnych  n-kach elemntow. Liczba n wynikać ma z liczby argumentów domknięcia.
+
+def list = (1..40) as Object
+
+eachTuple(list) { e1, e2, e3, e4 ->
+    println "$e1 $e2 $e3 $e4"
+}
+
+def s = 'asia ma kota i psa'
+
+def out = new StringBuffer()
+eachTuple(s) { a,b ->
+    out << b << a
+}
+println out
+
+static def eachTuple(list, closure) {
+    def n = closure.maximumNumberOfParameters
+    def batch = []
+    list.each {
+        batch << it
+        if (batch.size() == n) {
+            closure(batch)
+            batch.clear()
+        }
+    }
+}
